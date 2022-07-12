af36a6f63d2016f34b4115ba1e81cbdcadec8b7a
choi0926 <ghtjd753753@gmail.com>
2022-07-13 05:51:04 +0900

    Add : NumberExtraction

---
+++ 1.String/NumberExtraction.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class NumberExtraction {
+    public int solution(String str) {
+        int answer;
+        str = str.toUpperCase().replaceAll("[^0-9]","");
+        answer = Integer.parseInt(str);
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        NumberExtraction ne = new NumberExtraction();
+
+        Scanner keyboard = new Scanner(System.in);
+        String str = keyboard.next();
+        System.out.println(ne.solution(str));
+    }
+}
