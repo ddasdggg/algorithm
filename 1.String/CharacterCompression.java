6bc9a727170f38e0cd4fb64eb07a2adaea74c8d3
devchoi0926 <devchoi0926@gmail.com>
2022-07-16 04:58:36 +0900

    Add : CharacterCompression

---
+++ 1.String/CharacterCompression.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class CharacterCompression {
+    public String solution(String str) {
+        String answer = "";
+        str = str + " ";
+        int cnt = 1;
+        for (int i = 0; i < str.length() - 1; i++) {
+            if (str.charAt(i) == str.charAt(i + 1)) {
+                cnt++;
+            } else {
+                answer += str.charAt(i);
+                if (cnt > 1) {
+                    answer += String.valueOf(cnt);
+                    cnt = 1;
+                }
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        CharacterCompression cc = new CharacterCompression();
+
+        Scanner keyboard = new Scanner(System.in);
+        String str = keyboard.next();
+        System.out.print(cc.solution(str));
+    }
+}
